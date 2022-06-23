8d43b7ec3ac36086c561e26f541efcc36d0ec111
Augusto <guguponce91@gmail.com>
2022-06-24 00:40:33 +0200

    html style and logo in style

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
     },{
       test: /\.s?css$/,
       use: ["style-loader", "css-loader", "sass-loader"],
+    },
+    {
+      test: /\.(jpg|png|svg|gif)$/,
+      type: 'asset/resource',
     }
     ]
   },
