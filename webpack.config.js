5d43f0d72ac20fd85556f1601ad7b77423f94eb6
Augusto <guguponce91@gmail.com>
2022-06-27 21:48:17 +0200

    style editPage, header edited, total filtrado edited, twitterAuth added and set, emailAuth added, LoginPage edited

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,13 @@
-// const path = require('path');
-// module.exports = {
-//   mode: 'development',
-//   entry: ["babel-polyfill","./src/app.js"],
-//   output: {
-//     // path: path.join(__dirname, "public", "dist"),
-//     // filename: "bundle.js"
-//     path: path.resolve(__dirname, "public", "dist"),
-//     filename: "bundle.js",
-//     publicPath: '/'
-//   },
-//   module: {
-//     rules: [{
-//       loader: 'babel-loader',
-//       test: /\.js$/,
-//       exclude: /node_modules/
-//     },{
-//       test: /\.s?css$/,
-//       use: ["style-loader", "css-loader", "sass-loader"],
-//     },
-//     {
-//       test: /\.(jpg|png|svg|gif)$/,
-//       type: 'asset/resource',
-//     }
-//     ]
-//   },
-//   devtool: "eval-cheap-module-source-map",
-//   devServer: {
-//     historyApiFallback: true,
-//     static: {
-//       directory: path.join(__dirname, 'public', "dist"),
-//     },
-//     // historyApiFallback: true,
-//     compress: true,
-//     port: process.env.PORT || 3000,
-//   },
-// }
-
-
-//dev-server:
-
 const path = require('path');
-
 module.exports = {
   mode: 'development',
   entry: ["babel-polyfill","./src/app.js"],
   output: {
-    path: path.join(__dirname, "/public/dist"),
-      // path: path.resolve(__dirname, "public", "dist"),
+    // path: path.join(__dirname, "public", "dist"),
+    // filename: "bundle.js"
+    path: path.resolve(__dirname, "public", "dist"),
     filename: "bundle.js",
-    publicPath: "/"
+    publicPath: '/'
   },
   module: {
     rules: [{
@@ -69,15 +28,56 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     static: {
-      directory: path.join(__dirname, '/public'),
-      // directory: path.join(__dirname, "public","dist"),
+      directory: path.join(__dirname, 'public', "dist"),
     },
+    // historyApiFallback: true,
     compress: true,
     port: process.env.PORT || 3000,
   },
-  watch: true,
-  watchOptions: {
-    aggregateTimeout: 200,
-    poll: 1000,
-  },
 }
+
+
+// //dev-server:
+//
+// const path = require('path');
+//
+// module.exports = {
+//   mode: 'development',
+//   entry: ["babel-polyfill","./src/app.js"],
+//   output: {
+//     path: path.join(__dirname, "/public/dist"),
+//       // path: path.resolve(__dirname, "public", "dist"),
+//     filename: "bundle.js",
+//     publicPath: "/"
+//   },
+//   module: {
+//     rules: [{
+//       loader: 'babel-loader',
+//       test: /\.js$/,
+//       exclude: /node_modules/
+//     },{
+//       test: /\.s?css$/,
+//       use: ["style-loader", "css-loader", "sass-loader"],
+//     },
+//     {
+//       test: /\.(jpg|png|svg|gif)$/,
+//       type: 'asset/resource',
+//     }
+//     ]
+//   },
+//   devtool: "eval-cheap-module-source-map",
+//   devServer: {
+//     historyApiFallback: true,
+//     static: {
+//       directory: path.join(__dirname, '/public'),
+//       // directory: path.join(__dirname, "public","dist"),
+//     },
+//     compress: true,
+//     port: process.env.PORT || 3000,
+//   },
+//   watch: true,
+//   watchOptions: {
+//     aggregateTimeout: 200,
+//     poll: 1000,
+//   },
+// }
